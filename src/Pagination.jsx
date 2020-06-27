e8abb788406b6ca4f84b8bee355d2ff82fdaf73d
RomaKushnir <romaqwer08@gmail.com>
2020-06-27 21:31:56 +0300

    pagination

---
+++ src/Pagination.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Pagination extends React.Component {
+  // constructor() {
+  //   super();
+  // }
+
+  paginationPrev(currentPage) {
+    if (currentPage > 1) {
+      this.props.paginationClick(currentPage - 1);
+      console.log('prev',currentPage)
+    }
+  }
+
+  paginationNext(currentPage) {
+    if (currentPage < this.props.totalPages) {
+      this.props.paginationClick(currentPage + 1);
+    }
+  }
+
+  render() {
+    // console.log('pagination props', this.props)
+    const {paginationClick, currentPage, totalPages} = this.props;
+    return (
+      <div className="pagination-block p-5 row">
+        <button type="button" 
+          onClick={() => paginationClick(1)}>first</button>
+        <button type="button" 
+          onClick={() => this.paginationPrev(currentPage)}
+        >prev</button>
+        <button type="button" 
+          disabled className="active">{currentPage}</button>
+        <button type="button" 
+          onClick={() => this.paginationNext(currentPage)}>
+        next</button>
+        <button type="button" 
+          onClick= {() => paginationClick(totalPages-1)}>{`last ${totalPages-1}`}</button>
+      </div>
+    );
+  }  
+}
+
+export default Pagination;
