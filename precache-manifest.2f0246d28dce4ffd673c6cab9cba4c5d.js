05f711f48207a5d4af2b9131c42fabd032f5ae49
RomaKushnir <romaqwer08@gmail.com>
2020-07-06 21:40:41 +0300

    Updates

---
+++ precache-manifest.2f0246d28dce4ffd673c6cab9cba4c5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c7a2a68e984e9148b9fe064ae14ae7",
+    "url": "/movies_react/index.html"
+  },
+  {
+    "revision": "7045188ee750a4746182",
+    "url": "/movies_react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7ee58d9526b448531613",
+    "url": "/movies_react/static/css/main.242de2d2.chunk.css"
+  },
+  {
+    "revision": "7045188ee750a4746182",
+    "url": "/movies_react/static/js/2.1cd11e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies_react/static/js/2.1cd11e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee58d9526b448531613",
+    "url": "/movies_react/static/js/main.99af8ee6.chunk.js"
+  },
+  {
+    "revision": "709d742524b06b6e0c0c",
+    "url": "/movies_react/static/js/runtime-main.3ff82318.js"
+  },
+  {
+    "revision": "83255fa5c2a3c9b1d74e3de7951af0e8",
+    "url": "/movies_react/static/media/expand-more.83255fa5.svg"
+  }
+]);
--- src/apiMovieDB.js
+++
@@ -1,4 +0,0 @@
-export const API_key = '6e15d434ee1f8d03ce480c0399e7258d';
-export const API_url = 'https://api.themoviedb.org/3';
-
-
